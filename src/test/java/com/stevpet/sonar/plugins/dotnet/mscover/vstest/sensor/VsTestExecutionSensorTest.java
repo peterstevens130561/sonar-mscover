4a0e9331f4c61ba11396e68f2397029893ce222c
Peter Stevens <peter.stevens@bakerhughes.com>
2014-10-24 08:28:15 +0200

    VsTestExecutionSensor passes now, but not really with a test. Coverage at 75.2%

--- src/test/java/com/stevpet/sonar/plugins/dotnet/mscover/vstest/sensor/VsTestExecutionSensorTest.java
+++ src/test/java/com/stevpet/sonar/plugins/dotnet/mscover/vstest/sensor/VsTestExecutionSensorTest.java
@@ -2,14 +2,22 @@ package com.stevpet.sonar.plugins.dotnet.mscover.vstest.sensor;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 
 import org.junit.Test;
+import org.sonar.api.batch.SensorContext;
+import org.sonar.api.resources.Project;
 import org.sonar.api.scan.filesystem.ModuleFileSystem;
 import org.sonar.plugins.dotnet.api.microsoft.MicrosoftWindowsEnvironment;
 
 import com.stevpet.sonar.plugins.dotnet.mscover.MsCoverProperties;
+import com.stevpet.sonar.plugins.dotnet.mscover.PropertiesHelper;
 import com.stevpet.sonar.plugins.dotnet.mscover.vstest.results.VsTestEnvironment;
+import com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.AbstractVsTestRunnerFactory;
+import com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner;
+
 import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
 
 public class VsTestExecutionSensorTest {
 
@@ -18,12 +26,48 @@ public class VsTestExecutionSensorTest {
     private VsTestEnvironment vsTestEnvironment = mock(VsTestEnvironment.class);
     private ModuleFileSystem moduleFileSystem = mock(ModuleFileSystem.class);
     private MicrosoftWindowsEnvironment microsoftWindowsEnvironment = mock(MicrosoftWindowsEnvironment.class);
+    private Project project = mock(Project.class);
+    private SensorContext context = mock(SensorContext.class);
+    private AbstractVsTestRunnerFactory vsTestRunnerFactory = mock(AbstractVsTestRunnerFactory.class);
+    private VsTestRunner vsTestRunner = mock(VsTestRunner.class);
+    private MsCoverProperties propertiesHelper;
  
     
     @Test
     public void testVsTestExecutionSensor() {
         givenANewTestExecutionSensor();
         verifyThatTheTestExecutionSensorExists();
+    }
+    
+    @Test
+    public void runAnalysisForEmptyProject() {
+        givenANewTestExecutionSensor();
+        givenTestsHaveExecuted();
+        givenAnalysedProjectIsCSharpProject();
+        givenStubbedVsTestRunner();
+        
+        analyseProject();
+        
+    }
+
+    private void givenStubbedVsTestRunner() {
+        when(vsTestRunnerFactory.createBasicTestRunnner(any(PropertiesHelper.class), 
+                any(ModuleFileSystem.class), 
+                any(MicrosoftWindowsEnvironment.class)
+                )
+                ).thenReturn(vsTestRunner);
+    }
+
+    private void analyseProject() {
+        sensor.analyse(project, context);
+    }
+
+    private void givenAnalysedProjectIsCSharpProject() {
+        when(project.isRoot()).thenReturn(false);
+        when(moduleFileSystem.baseDir()).thenReturn(new File("mysolution/myproject"));
+    }
+
+    private void givenTestsHaveExecuted() {
     }
 
     private void verifyThatTheTestExecutionSensorExists() {
@@ -32,6 +76,7 @@ public class VsTestExecutionSensorTest {
 
     private void givenANewTestExecutionSensor() {
         sensor=new VsTestExecutionSensor(vsTestEnvironment,msCoverProperties,moduleFileSystem,microsoftWindowsEnvironment);
+        sensor.setVsTestRunnerFactory(vsTestRunnerFactory);
     }
 
     @Test
