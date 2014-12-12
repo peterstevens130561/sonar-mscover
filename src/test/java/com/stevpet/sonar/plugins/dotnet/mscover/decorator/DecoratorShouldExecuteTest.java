bb665b788a904eb40e339c41f5bce2ce08cbda77
Peter Stevens <peter.stevens@bakerhughes.com>
2014-12-12 21:03:14 +0100

    Cleaned DecoratorShouldExecuteTest

--- src/test/java/com/stevpet/sonar/plugins/dotnet/mscover/decorator/DecoratorShouldExecuteTest.java
+++ src/test/java/com/stevpet/sonar/plugins/dotnet/mscover/decorator/DecoratorShouldExecuteTest.java
@@ -1,52 +1,46 @@
 package com.stevpet.sonar.plugins.dotnet.mscover.decorator;
 
-
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.sonar.api.batch.Decorator;
-import org.sonar.api.config.Settings;
 
 import com.stevpet.sonar.plugins.dotnet.mscover.PropertiesHelper;
+import com.stevpet.sonar.plugins.dotnet.mscover.sonarmocks.SettingsMock;
 
 public class DecoratorShouldExecuteTest {
 
+    SettingsMock settingsMock = new SettingsMock();
     @Test
     public void shouldExecute_PropertyNotSet_Disabled() {
-        //Arrange
-        Settings settings = mock(Settings.class);
-        when(settings.getString(PropertiesHelper.MSCOVER_INTEGRATION_COVERAGEXML_PATH)).thenReturn(null);
-        Decorator decorator = new CoverageDecoratorStub(settings);
-        //Act
+        //Given
+        settingsMock.givenString(PropertiesHelper.MSCOVER_INTEGRATION_COVERAGEXML_PATH,null);
+      
+        Decorator decorator = new CoverageDecoratorStub(settingsMock.getMock());
+        //When
         boolean shouldExecute = decorator.shouldExecuteOnProject(null);
-        //Assert
+        //Then
         Assert.assertFalse(shouldExecute);
     }
     
     @Test
     public void shouldExecute_PropertySet_Enabled() {
-        //Arrange
-        Settings settings = mock(Settings.class);
-        when(settings.getString(PropertiesHelper.MSCOVER_INTEGRATION_COVERAGEXML_PATH)).thenReturn("a/b/c");
-        Decorator decorator = new CoverageDecoratorStub(settings);
-        //Act
+        //Given
+        settingsMock.givenString(PropertiesHelper.MSCOVER_INTEGRATION_COVERAGEXML_PATH,"a/b/c");
+        Decorator decorator = new CoverageDecoratorStub(settingsMock.getMock());
+        //When
         boolean shouldExecute = decorator.shouldExecuteOnProject(null);
-        //Assert
+        //Then
         Assert.assertTrue(shouldExecute);
     }
     
     @Test 
     public void shouldExecuteIntegrationTests_PropertySet_Enabled() {
-        //Arrange
-        Settings settings = mock(Settings.class);
-        when(settings.getString(PropertiesHelper.MSCOVER_INTEGRATION_COVERAGEXML_PATH)).thenReturn("a/b/c");
-        Decorator decorate = new IntegrationTestLineDecorator(settings,null);
-        //Act
+        //Given
+        settingsMock.givenString(PropertiesHelper.MSCOVER_INTEGRATION_COVERAGEXML_PATH, "a/b/c");
+        Decorator decorate = new IntegrationTestLineDecorator(settingsMock.getMock(),null);
+        //When
         boolean actual = decorate.shouldExecuteOnProject(null);
-        //Assert
+        //Then
         Assert.assertTrue(actual);
     }
     
---
+++ src/test/java/com/stevpet/sonar/plugins/dotnet/mscover/sonarmocks/SettingsMock.java
@@ -0,0 +1,19 @@
+package com.stevpet.sonar.plugins.dotnet.mscover.sonarmocks;
+
+import static org.mockito.Mockito.when;
+
+import org.sonar.api.config.Settings;
+import com.stevpet.sonar.plugins.dotnet.mscover.mock.GenericClassMock;
+
+public class SettingsMock extends GenericClassMock<Settings> {
+
+    public SettingsMock() {
+        super(Settings.class);
+    }
+
+    public void givenString(String key,
+            String value) {
+        when(instance.getString(key)).thenReturn(value);
+    }
+
+}
