70a9fb0b0dd70dfcd68618e37966b66f9450b50a
Peter Stevens <peter.stevens@bakerhughes.com>
2015-03-31 14:41:03 +0200

    Minor cleanup, alll dependencies are gone

--- src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/vstest/runner/WindowsVsTestRunner.java
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/vstest/runner/WindowsVsTestRunner.java
@@ -45,213 +45,228 @@ import com.stevpet.sonar.plugins.dotnet.mscover.vstest.command.VSTestCommand;
 import com.stevpet.sonar.plugins.dotnet.mscover.vstest.exceptions.MsCoverInvalidSonarWorkingDir;
 import com.stevpet.sonar.plugins.dotnet.mscover.vstest.results.VSTestStdOutParser;
 
-
 /**
  * @author stevpet
- *
+ * 
  */
-public class WindowsVsTestRunner extends  VsTestRunnerCommandBuilder {
+public class WindowsVsTestRunner extends VsTestRunnerCommandBuilder {
     private static final Logger LOG = LoggerFactory
             .getLogger(WindowsVsTestRunner.class);
-    private MsCoverProperties propertiesHelper ;
+    private MsCoverProperties propertiesHelper;
     private List<String> unitTestAssembliesPath;
     private String coveragePath;
     private String resultsPath;
     private File testSettingsFile;
-    
+
     private String sonarPath;
     private boolean doCodeCoverage;
     private String stdOutString;
     private CodeCoverageCommand codeCoverageCommand;
     private TestConfigFinder testConfigFinder;
     private VSTestCommand vsTestCommand;
-    private CommandLineExecutor executor = new WindowsCommandLineExecutor();
+    private CommandLineExecutor executor;
     private VSTestStdOutParser vsTestStdOutParser;
     private MicrosoftWindowsEnvironment microsoftWindowsEnvironment;
     private FileSystem fileSystem;
     private AssembliesFinder assembliesFinder;
+
     private WindowsVsTestRunner() {
     }
-    
-    public WindowsVsTestRunner(MsCoverProperties propertiesHelper, 
-            MicrosoftWindowsEnvironment microsoftWindowsEnvironment, 
-            FileSystem fileSystem,
-            CodeCoverageCommand codeCoverageCommand,
-            TestConfigFinder testConfigFinder,
-            VSTestCommand vsTestCommand, 
-            CommandLineExecutor commandLineExecutor, 
-            VSTestStdOutParser vsTestStdOutParser, AssembliesFinder assembliesFinder) {
+
+    public WindowsVsTestRunner(MsCoverProperties propertiesHelper,
+            MicrosoftWindowsEnvironment microsoftWindowsEnvironment,
+            FileSystem fileSystem, CodeCoverageCommand codeCoverageCommand,
+            TestConfigFinder testConfigFinder, VSTestCommand vsTestCommand,
+            CommandLineExecutor commandLineExecutor,
+            VSTestStdOutParser vsTestStdOutParser,
+            AssembliesFinder assembliesFinder) {
         this.propertiesHelper = propertiesHelper;
         this.microsoftWindowsEnvironment = microsoftWindowsEnvironment;
-        this.fileSystem=fileSystem;
-        this.codeCoverageCommand=codeCoverageCommand;
-        this.testConfigFinder=testConfigFinder;
-        this.vsTestCommand=vsTestCommand;
-                this.executor=commandLineExecutor;
-                this.vsTestStdOutParser=vsTestStdOutParser;
-                this.assembliesFinder = assembliesFinder;
+        this.fileSystem = fileSystem;
+        this.codeCoverageCommand = codeCoverageCommand;
+        this.testConfigFinder = testConfigFinder;
+        this.vsTestCommand = vsTestCommand;
+        this.executor = commandLineExecutor;
+        this.vsTestStdOutParser = vsTestStdOutParser;
+        this.assembliesFinder = assembliesFinder;
     }
+
     public static VsTestRunner create() {
         return new WindowsVsTestRunner();
     }
 
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#setSonarPath(java.lang.String)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#
+     * setSonarPath(java.lang.String)
      */
 
-        
-    
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#getSolutionDirectory()
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#
+     * getSolutionDirectory()
      */
     private File getSolutionDirectory() {
-        VisualStudioSolution solution=microsoftWindowsEnvironment.getCurrentSolution();
-        if(solution==null) {
+        VisualStudioSolution solution = microsoftWindowsEnvironment
+                .getCurrentSolution();
+        if (solution == null) {
             throw new SonarException("No current solution");
         }
         return solution.getSolutionDir();
     }
-    
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#setDoCodeCoverage(boolean)
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#
+     * setDoCodeCoverage(boolean)
      */
     public void setDoCodeCoverage(boolean doCodeCoverage) {
-        this.doCodeCoverage=doCodeCoverage;
+        this.doCodeCoverage = doCodeCoverage;
     }
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#shouldRun()
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#shouldRun
+     * ()
      */
     public boolean shouldRun() {
-        boolean shouldRun=propertiesHelper.getRunMode() == RunMode.RUNVSTEST;
-        LOG.debug("shouldRun ->{}",shouldRun);
+        boolean shouldRun = propertiesHelper.getRunMode() == RunMode.RUNVSTEST;
+        LOG.debug("shouldRun ->{}", shouldRun);
         return shouldRun;
-    } 
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#runTests()
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#runTests
+     * ()
      */
     public void execute() {
-        VSTestCommand vsTestCommand=build();
+        VSTestCommand vsTestCommand = build();
         executeShellCommand(vsTestCommand);
         getResultPaths();
-        if(doCodeCoverage) {
+        if (doCodeCoverage) {
             convertCoverageFileToXml();
         }
     }
-    
-    
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#prepareTestCommand()
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#
+     * prepareTestCommand()
      */
     public VSTestCommand build() {
         requireTestSettings();
         findAssemblies();
-        return buildVSTestCommand();     
+        return buildVSTestCommand();
     }
 
     /**
      * Converts the .coverage file into an xml file
      */
-    
-    
+
     private void convertCoverageFileToXml() {
-            String sonarPath=fileSystem.workDir().getAbsolutePath();
-            codeCoverageCommand.setSonarPath(sonarPath);
-            codeCoverageCommand.setCoveragePath(coveragePath);
-            codeCoverageCommand.setOutputPath(getCoverageXmlPath());
-            codeCoverageCommand.install();
-            executeShellCommand(codeCoverageCommand);
+        String sonarPath = fileSystem.workDir().getAbsolutePath();
+        codeCoverageCommand.setSonarPath(sonarPath);
+        codeCoverageCommand.setCoveragePath(coveragePath);
+        codeCoverageCommand.setOutputPath(getCoverageXmlPath());
+        codeCoverageCommand.install();
+        executeShellCommand(codeCoverageCommand);
     }
-   
+
     public String getCoverageXmlPath() {
-        String path=propertiesHelper.getUnitTestCoveragePath() ;
-        if(StringUtils.isEmpty(path)) {
-            path=fileSystem.workDir().getAbsolutePath() + "/coverage.xml";
+        String path = propertiesHelper.getUnitTestCoveragePath();
+        if (StringUtils.isEmpty(path)) {
+            path = fileSystem.workDir().getAbsolutePath() + "/coverage.xml";
         }
         return path;
     }
 
-
     private void requireTestSettings() {
         String testSettings = propertiesHelper.getTestSettings();
-        testSettingsFile = testConfigFinder.getTestSettingsFileOrDie(getSolutionDirectory(),testSettings);
-  
+        testSettingsFile = testConfigFinder.getTestSettingsFileOrDie(
+                getSolutionDirectory(), testSettings);
+
     }
 
-    
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#getResultPaths()
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#
+     * getResultPaths()
      */
     private void getResultPaths() {
         vsTestStdOutParser.setResults(stdOutString);
         setCoveragePath(vsTestStdOutParser.getCoveragePath());
-        this.resultsPath = vsTestStdOutParser.getTestResultsXmlPath();    
+        this.resultsPath = vsTestStdOutParser.getTestResultsXmlPath();
     }
 
     private int executeShellCommand(ShellCommand command) {
-        int exitCode= executor.execute(command);
-        stdOutString=executor.getStdOut();
+        int exitCode = executor.execute(command);
+        stdOutString = executor.getStdOut();
         return exitCode;
     }
-    
-    
+
     private VSTestCommand buildVSTestCommand() {
         vsTestCommand.setTestSettingsFile(testSettingsFile);
         vsTestCommand.setUnitTestAssembliesPath(unitTestAssembliesPath);
         vsTestCommand.setCodeCoverage(doCodeCoverage);
-        String platform=propertiesHelper.getRequiredBuildPlatform();
+        String platform = propertiesHelper.getRequiredBuildPlatform();
         vsTestCommand.setPlatform(platform);
         return vsTestCommand;
     }
-    
 
     private void findAssemblies() {
-        unitTestAssembliesPath=assembliesFinder.findUnitTestAssembliesFromConfig(getSolutionDirectory(), microsoftWindowsEnvironment.getCurrentSolution().getProjects());
+        unitTestAssembliesPath = assembliesFinder
+                .findUnitTestAssembliesFromConfig(getSolutionDirectory(),
+                        microsoftWindowsEnvironment.getCurrentSolution()
+                                .getProjects());
     }
 
-    
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#setCoveragePath(java.lang.String)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#
+     * setCoveragePath(java.lang.String)
      */
     private void setCoveragePath(String coveragePath) {
         this.coveragePath = coveragePath;
     }
 
-
-    /* (non-Javadoc)
-     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#getResultsXmlPath()
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.stevpet.sonar.plugins.dotnet.mscover.vstest.runner.VsTestRunner#
+     * getResultsXmlPath()
      */
     public String getResultsXmlPath() {
         return resultsPath;
     }
 
-
     void setTestConfigFinder(TestConfigFinder testConfigFinder) {
         this.testConfigFinder = testConfigFinder;
     }
-    
+
     public void clean() {
-        File sonarDir=fileSystem.workDir();
-        if(sonarDir==null) {
+        File sonarDir = fileSystem.workDir();
+        if (sonarDir == null) {
             throw new MsCoverProgrammerException("sonarPath not set");
         }
-        if(!".sonar".equalsIgnoreCase(sonarDir.getName())) {
+        if (!".sonar".equalsIgnoreCase(sonarDir.getName())) {
             throw new MsCoverInvalidSonarWorkingDir(sonarPath);
         }
-        File testResultsDir=new File(sonarPath,"TestResults");
-        if(testResultsDir.exists()) {
+        File testResultsDir = new File(sonarPath, "TestResults");
+        if (testResultsDir.exists()) {
             FileUtils.deleteQuietly(testResultsDir);
         }
 
     }
-    
-    /**
-     * Set the coveragecommand (testing purposes only)
-     * @param command
-     */
-    void setCoverageCommand(CodeCoverageCommand command) {
-        this.codeCoverageCommand = command;
-    }
-
 
 }
-    
