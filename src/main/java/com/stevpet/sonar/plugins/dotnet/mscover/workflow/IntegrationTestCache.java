c52180a6a4466838530670d436ef905a83721f08
Peter Stevens <peter.stevens@bakerhughes.com>
2016-01-24 13:40:41 +0100

    Enhanced integratontestcache to support keepimng track of the first module

--- src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/workflow/IntegrationTestCache.java
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/workflow/IntegrationTestCache.java
@@ -1,7 +1,11 @@
 package com.stevpet.sonar.plugins.dotnet.mscover.workflow;
 
+import javax.annotation.Nonnull;
+
 import org.sonar.api.BatchExtension;
 import org.sonar.api.batch.InstantiationStrategy;
+
+import com.google.common.base.Preconditions;
 
 /**
  * Should be used by all integration test sensors
@@ -10,5 +14,16 @@ import org.sonar.api.batch.InstantiationStrategy;
  */
 @InstantiationStrategy(InstantiationStrategy.PER_BATCH)
 public class IntegrationTestCache extends TestCacheBase implements BatchExtension{
+
+	private String firstModule;
+
+	public void setFirstModule(@Nonnull String firstModule) {
+		this.firstModule=firstModule;
+	}
+	
+	public String getFirstModule() {
+		Preconditions.checkNotNull(firstModule);
+		return firstModule;
+	}
     
 }
