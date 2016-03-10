e72a26ef926aa9f3ee40d63acc62668e36ff6757
Peter Stevens <peter.stevens@bakerhughes.com>
2016-03-10 06:35:39 +0100

    BranchCoverageMetrics - interface added

---
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/coveragesaver/defaultsaver/BranchCoverageMetrics.java
@@ -0,0 +1,11 @@
+package com.stevpet.sonar.plugins.dotnet.mscover.coveragesaver.defaultsaver;
+
+import org.sonar.api.measures.Metric;
+
+public interface BranchCoverageMetrics {
+    Metric getCoveredByLineMetric();
+    Metric getToCoverByLineMetric();
+    Metric getCoverageMetric() ;
+    Metric getToCoverMetric() ;
+    Metric getUncoveredMetric() ;
+}
