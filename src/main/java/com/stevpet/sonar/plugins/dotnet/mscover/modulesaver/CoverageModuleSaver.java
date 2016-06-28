f5555889091e5e3a3dc3c7686b240eb9e111b07f
Peter Stevens <peter.stevens@bakerhughes.com>
2016-06-28 06:41:00 +0200

    Refactoring CoverageModuleSaver completed

--- src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/modulesaver/CoverageModuleSaver.java
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/modulesaver/CoverageModuleSaver.java
@@ -5,23 +5,7 @@ import java.io.File;
 
 
 interface CoverageModuleSaver  {
-    /**
-     * set the name of the project being analysed, will determine the name of the created xml files
-     * @param projectName
-     * @return
-     */
-    CoverageModuleSaver setProject(String projectName);
-
-    /**
-     * set directory below which the coverage directories will be created
-     * @param root
-     * @return
-     */
-    CoverageModuleSaver setDirectory(File root);
-	/**
-	 * 
-	 * @param sb fully qualified xml document
-	 */
-	void save(String sb);
+    
+    void save(File coverageRootDir, String testProjectName, String xml);
 
 }
--- src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/modulesaver/OpenCoverCoverageModuleSaver.java
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/modulesaver/OpenCoverCoverageModuleSaver.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+
 import javax.annotation.Nonnull;
 
 import com.google.common.base.Preconditions;
@@ -13,36 +14,24 @@ class OpenCoverCoverageModuleSaver implements CoverageModuleSaver {
 
 	
 	private ModuleParser moduleParser;
-	private File root;
-	private String projectName;
 	private final CoverageFileLocator coverageFileLocator;
 	OpenCoverCoverageModuleSaver(@Nonnull ModuleParser moduleParser) {
 		this.moduleParser = moduleParser;
 		this.coverageFileLocator = new DefaultCoverageFileLocator();
 	}
 	
-	@Override 
-	public CoverageModuleSaver setProject(@Nonnull String projectName) {
-		this.projectName = projectName;
-		return this;
-	}
 	
-	@Override
-	public CoverageModuleSaver setDirectory(@Nonnull File root) {
-		this.root=root;
-		return this;
-	}
-	
-	@Override
-	public void save(String xmlDoc)  {
-		Preconditions.checkNotNull(projectName,"project not set");
-		Preconditions.checkNotNull(root,"root not set");
+    @Override
+    public void save(@Nonnull File coverageRootDir,  @Nonnull String testProjectName,@Nonnull String xmlDoc) {
+		Preconditions.checkNotNull(testProjectName,"project not set");
+		Preconditions.checkNotNull(coverageRootDir,"root not set");
+	      Preconditions.checkNotNull(xmlDoc,"root not set");
 	      moduleParser.parse(xmlDoc);
 	        if(moduleParser.getSkipped() || moduleParser.isNotCovered()) {
 	            return;
 	        }
 		String moduleName = moduleParser.getModuleName();
-		File artifactFile= coverageFileLocator.getFile(root, projectName, moduleName);
+		File artifactFile= coverageFileLocator.getFile(coverageRootDir, testProjectName, moduleName);
 		createModuleDir(artifactFile);	
 		saveModule(artifactFile,xmlDoc);
 	}
@@ -72,6 +61,8 @@ class OpenCoverCoverageModuleSaver implements CoverageModuleSaver {
 	    
 		return moduleDir;
 	}
+
+
 		
 
 }
