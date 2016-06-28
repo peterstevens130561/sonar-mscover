a04526e2a3c251e2d2ce9e3822a7397e7eced7ed
Peter Stevens <peter.stevens@bakerhughes.com>
2016-06-28 06:54:41 +0200

    CoverageModuleSaver: Add comments

--- src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/modulesaver/CoverageModuleSaver.java
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/modulesaver/CoverageModuleSaver.java
@@ -5,7 +5,15 @@ import java.io.File;
 
 
 interface CoverageModuleSaver  {
-    
+    /**
+     * The xml doc is stored in directory below coverageRootDir, name of directory is the module in the xml doc
+     * and file is named after the testProjectName.
+     * <coverageRootDir>/<module>/<testProjectName>.xml
+     *
+     * @param coverageRootDir - root
+     * @param testProjectName - name of the file, use the name of the test project to make sure it is unique
+     * @param xml - fully qualified xml doc
+     */
     void save(File coverageRootDir, String testProjectName, String xml);
 
 }
