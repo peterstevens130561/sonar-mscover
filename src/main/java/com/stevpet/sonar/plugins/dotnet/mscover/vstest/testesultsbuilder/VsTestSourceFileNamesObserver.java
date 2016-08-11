f48a5a34ff31f677cfe807716a822406e2a6a322
Peter Stevens <peter.stevens@bakerhughes.com>
2016-08-11 09:25:59 +0200

    Another one fully converted. All tests pass

--- src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/vstest/testesultsbuilder/VsTestSourceFileNamesObserver.java
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/vstest/testesultsbuilder/VsTestSourceFileNamesObserver.java
@@ -1,7 +1,6 @@
 package com.stevpet.sonar.plugins.dotnet.mscover.vstest.testesultsbuilder;
 
 import com.stevpet.sonar.plugins.common.api.parser.BaseParserObserver;
-import com.stevpet.sonar.plugins.common.api.parser.annotations.ElementMatcher;
 import com.stevpet.sonar.plugins.common.parser.ObserverRegistrar;
 import com.stevpet.sonar.plugins.dotnet.mscover.model.SourceFileNameTable;
 
@@ -9,29 +8,28 @@ public class VsTestSourceFileNamesObserver extends BaseParserObserver {
     private SourceFileNameTable sourceFileNameTable;
     private String sourceFileID;
 
+    @Override
+    public void registerObservers(ObserverRegistrar registrar) {
+        registrar.onElement(this::sourceFileIDMatcher, "SourceFileID")
+        .onElement(this::sourceFileNameMatcher, "SourceFileName");
+    }
+    
     public void setRegistry(SourceFileNameTable sourceFileNameTable) {
         this.sourceFileNameTable = sourceFileNameTable;
-
     }
 
     public VsTestSourceFileNamesObserver() {
         setPattern("SourceFileNames/(SourceFileID|SourceFileName)");
     }
 
-    @ElementMatcher(elementName = "SourceFileID")
     public void sourceFileIDMatcher(String value) {
         sourceFileID = value;
     }
 
-    @ElementMatcher(elementName = "SourceFileName")
     public void sourceFileNameMatcher(String sourceFileName) {
         sourceFileNameTable.add(sourceFileID, sourceFileName);
     }
 
-    @Override
-    public void registerObservers(ObserverRegistrar registrar) {
-        // TODO Auto-generated method stub
-        
-    }
+
 
 }
