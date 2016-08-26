9d11fae92b1a9d56357fb88647129b2ff0775ce8
Peter Stevens <peter.stevens@bakerhughes.com>
2016-08-26 15:00:32 +0200

    Add some documentation

--- src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/coveragesaver/defaultsaver/BranchCoverageMetrics.java
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/mscover/coveragesaver/defaultsaver/BranchCoverageMetrics.java
@@ -3,9 +3,31 @@ package com.stevpet.sonar.plugins.dotnet.mscover.coveragesaver.defaultsaver;
 import org.sonar.api.measures.Metric;
 
 public interface BranchCoverageMetrics {
+    /**
+     * array of strings showing paths covered at line level
+     * @return
+     */
     Metric<String> getCoveredByLineMetric();
+    /**
+     * array of strings showing number of paths to cover by line
+     * @return
+     */
     Metric<String> getToCoverByLineMetric();
+    
+    /**
+     * percentage
+     * @return
+     */
     Metric<Double> getCoverageMetric() ;
+    
+    /**
+     * number of branches to cover
+     * @return
+     */
     Metric<Integer> getToCoverMetric() ;
+    /**
+     * number of branches covered
+     * @return
+     */
     Metric<Integer> getUncoveredMetric() ;
 }
